9c962a0a8b9de1410cbb8c80f67253124ec00f63
nuhptr <nugrahaadi733@gmail.com>
2023-10-12 09:17:38 +0700

    done react refresher

--- react-refresher/src/App.js
+++ react-refresher/src/App.js
@@ -1,17 +1,25 @@
+import { useState } from 'react'
+
 import GoalList from './components/goal-list'
+import NewGoal from './components/new-goal'
 
 import './App.css'
 
 export default function App() {
-   const courseGoals = [
+   const [courseGoals, setCourseGoals] = useState([
       { id: 'cg1', text: 'Finish the course' },
       { id: 'cg2', text: 'Learn all about the course main topic' },
       { id: 'cg3', text: 'Help other students in the course Q&A' },
-   ]
+   ])
+
+   const addNewGoalHandler = (newGoal) => {
+      setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal))
+   }
 
    return (
       <div>
          <h2 className='goal-title'>Course Goals</h2>
+         <NewGoal onAddGoal={addNewGoalHandler} />
          <GoalList goals={courseGoals} />
       </div>
    )
---
+++ react-refresher/src/components/new-goal.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import './new-goal.css'
+
+export default function NewGoal({ onAddGoal }) {
+   const [enteredValue, setEnteredValue] = useState('')
+
+   const addGoalHandler = (event) => {
+      event.preventDefault()
+
+      const newGoal = { id: Math.random().toString(), text: enteredValue }
+      console.log(newGoal)
+
+      onAddGoal(newGoal)
+      setEnteredValue('')
+   }
+
+   const textChangeHandler = (event) => {
+      setEnteredValue(event.target.value)
+   }
+
+   return (
+      <form className='new-goal' onSubmit={addGoalHandler}>
+         <input type='text' value={enteredValue} onChange={textChangeHandler} />
+         <button type='submit'>Add Goal</button>
+      </form>
+   )
+}
