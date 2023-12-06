0e1b119568f10f8d5112119cbae8db2467818e13
nuhptr <nugrahaadi733@gmail.com>
2023-12-07 00:18:51 +0700

    src/app update code

--- sharehub/src/App.jsx
+++ sharehub/src/App.jsx
@@ -1,31 +1,17 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+import React from "react"
+import { RouterProvider, Navigate, createBrowserRouter } from "react-router-dom"
 
-function App() {
-   const [count, setCount] = useState(0)
+import Users from "./user/pages/users"
+import NewPlaces from "./places/pages/new-places"
 
-   return (
-      <>
-         <div>
-            <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-               <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank" rel="noreferrer">
-               <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-         </div>
-         <h1>Vite + React</h1>
-         <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-            <p>
-               Edit <code>src/App.jsx</code> and save to test HMR
-            </p>
-         </div>
-         <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      </>
-   )
+const router = createBrowserRouter([
+   { path: "/", element: <Users />, exact: true },
+   { path: "places/new", element: <NewPlaces /> },
+
+   // Redirects
+   { path: "*", element: <Navigate to={"/"} /> },
+])
+
+export default function App() {
+   return <RouterProvider router={router} />
 }
-
-export default App
