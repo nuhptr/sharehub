cd49d06bec816d0031899c2ddb811a3704f8fd99
nuhptr <nugrahaadi733@gmail.com>
2023-12-07 00:20:26 +0700

    src/users add pages and components

---
+++ sharehub/src/user/components/user-item.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import "./user-item.css"
+
+export default function UserItem() {
+   return <div>UserItem</div>
+}
---
+++ sharehub/src/user/components/users-list.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import "./users-list.css"
+
+export default function UsersList() {
+   return <div>UsersList</div>
+}
