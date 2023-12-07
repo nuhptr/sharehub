03a478a3cd394498311fb89205626d5e07c5e623
nuhptr <nugrahaadi733@gmail.com>
2023-12-07 10:31:27 +0700

    users/update code

--- sharehub/src/user/components/users-list.jsx
+++ sharehub/src/user/components/users-list.jsx
@@ -1,7 +1,21 @@
-import React from "react"
-
 import "./users-list.css"
 
-export default function UsersList() {
-   return <div>UsersList</div>
+import UserItem from "./user-item"
+
+export default function UsersList({ items }) {
+   if (items.length === 0) {
+      return (
+         <div className="center">
+            <h2>No users found!</h2>
+         </div>
+      )
+   }
+
+   return (
+      <ul className="users-list">
+         {items.map((user) => (
+            <UserItem key={user.id} {...user} />
+         ))}
+      </ul>
+   )
 }
--- sharehub/src/user/pages/users.jsx
+++ sharehub/src/user/pages/users.jsx
@@ -1,5 +1,14 @@
-import React from "react"
+import UsersList from "../components/users-list"
 
 export default function Users() {
-   return <div>Users works!</div>
+   const USERS = [
+      {
+         id: "u1",
+         name: "Max Schwarz",
+         image: "https://i.pinimg.com/564x/4e/2f/73/4e2f736d891f0e6ed9f5d4fd0e374f90.jpg",
+         places: 3,
+      },
+   ]
+
+   return <UsersList items={USERS} />
 }
