3ff6cc35e084c736026171daa2553947c76f4302
nuhptr <nugrahaadi733@gmail.com>
2023-12-07 15:27:55 +0700

    ./route update

--- sharehub/src/App.jsx
+++ sharehub/src/App.jsx
@@ -1,38 +1,20 @@
-import { RouterProvider, Navigate, createBrowserRouter } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 import Users from "./user/pages/users"
 import NewPlaces from "./places/pages/new-places"
 import MainNavigation from "./shared/components/main-navigation"
 
-const router = createBrowserRouter([
-   {
-      path: "/",
-      exact: true,
-      element: (
-         <>
-            <MainNavigation />
-            <Users />
-         </>
-      ),
-   },
-   {
-      path: "places/new",
-      element: (
-         <>
-            <MainNavigation />
-            <NewPlaces />
-         </>
-      ),
-   },
-
-   // Redirects
-   { path: "*", element: <Navigate to="/" /> },
-])
-
 export default function App() {
    return (
-      <main>
-         <RouterProvider router={router} />
-      </main>
+      <div>
+         <MainNavigation />
+         <main>
+            <Routes>
+               <Route path="/" exact element={<Users />} />
+               <Route path="/places/new" exact element={<NewPlaces />} />
+               <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+         </main>
+      </div>
    )
 }
