17fdc726a1d88762be2fa530d4573a35890e4d2a
nuhptr <nugrahaadi733@gmail.com>
2024-01-23 13:40:47 +0700

    app:apply react-router-dom

--- sharehub-client/src/App.js
+++ sharehub-client/src/App.js
@@ -1,5 +1,20 @@
 import React from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
-export default function App() {
-   return <h1 title="This Works">Hi, this is ReactJS</h1> // React.createElement('h1', {title: 'This Works'}, 'Hi, this is ReactJS')
+import Users from "./user/pages/Users"
+import NewPlace from "./places/pages/NewPlace"
+
+// React.createElement('h1', {title: 'This Works'}, 'Hi, this is ReactJS')
+const App = () => {
+   return (
+      <BrowserRouter future={{ v7_startTransition: true }}>
+         <Routes>
+            <Route path="/" element={<Users />} exact />
+            <Route path="/places/new" element={<NewPlace />} exact />
+            <Route path="*" element={<Navigate to="/" replace />} />
+         </Routes>
+      </BrowserRouter>
+   )
 }
+
+export default App
