4a94301a2def620b8e7f864102dd36017d6071cc
nuhptr <nugrahaadi733@gmail.com>
2024-01-25 20:10:50 +0700

    add map, yahh walaupun gabayar yang penting bisa cara makenya

--- sharehub-client/src/components/atomic/Maps.jsx
+++ sharehub-client/src/components/atomic/Maps.jsx
@@ -1,16 +1,19 @@
-import React, { useRef } from "react"
+import React, { useRef, useEffect } from "react"
 
 import "./Maps.css"
 
 export default function Maps({ className, style, center, zoom }) {
    const mapRef = useRef()
 
-   const map = new window.google.maps.Map(mapRef.current, {
-      center: center,
-      zoom: zoom,
-   })
+   useEffect(() => {
+      //* add map to the DOM
+      const map = new window.google.maps.Map(mapRef.current, {
+         center: center,
+         zoom: zoom,
+      })
 
-   new window.google.maps.Marker({ position: center, map: map })
+      new window.google.maps.Marker({ position: center, map: map })
+   }, [center, zoom])
 
-   return <div ref={mapRef} className={`map ${className}`} style={style}></div>
+   return <div ref={mapRef} className={`map ${className}`} style={style} id="map"></div>
 }
