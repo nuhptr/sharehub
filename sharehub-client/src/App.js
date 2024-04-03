0a002943819ecb49683aae61327bd2b812eebd87
nuhptr <nugrahaadi733@gmail.com>
2024-04-03 17:17:37 +0700

    src: update some code

--- sharehub-client/src/App.js
+++ sharehub-client/src/App.js
@@ -1,23 +1,27 @@
-import React from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { styled } from "styled-components"
 
-import Users from "./pages/Users"
-import NewPlace from "./pages/NewPlace"
-import MainNavigation from "./components/navigation/MainNavigation"
-import UserPlace from "./pages/UserPlace"
+import Users from "./pages/user/users"
+import NewPlace from "./pages/place/new-place"
+import MainNavigation from "./components/navigation/main-navigation"
+import UserPlace from "./pages/user-place/user-place"
+
+const StyledMain = styled.main`
+    margin-top: 6.5rem;
+`
 
 export default function App() {
-   return (
-      <BrowserRouter future={{ v7_startTransition: true }}>
-         <MainNavigation />
-         <main>
-            <Routes>
-               <Route path="/" element={<Users />} exact />
-               <Route path="/:userId/places" element={<UserPlace />} />
-               <Route path="/places/new" element={<NewPlace />} />
-               <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-         </main>
-      </BrowserRouter>
-   )
+    return (
+        <BrowserRouter future={{ v7_startTransition: true }}>
+            <MainNavigation />
+            <StyledMain>
+                <Routes>
+                    <Route path="/" element={<Users />} exact />
+                    <Route path="/:userId/places" element={<UserPlace />} />
+                    <Route path="/places/new" element={<NewPlace />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </StyledMain>
+        </BrowserRouter>
+    )
 }
