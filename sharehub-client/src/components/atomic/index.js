d41dfc5c5fd66643e0bf785df3122a674258752e
nuhptr <nugrahaadi733@gmail.com>
2024-04-08 15:12:02 +0700

    add entry point component

---
+++ sharehub-client/src/components/atomic/index.js
@@ -0,0 +1,9 @@
+import Avatar from "./avatar"
+import Button from "./button"
+import Card from "./card"
+import Input from "./input"
+import Modal from "./modal"
+import Backdrop from "./backdrop"
+import Maps from "./maps"
+
+export { Avatar, Button, Card, Input, Modal, Backdrop, Maps }
