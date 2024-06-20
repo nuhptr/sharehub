20da690e20271583b7755c4466def66cbaa8f36e
nuhptr <nugrahaadi733@gmail.com>
2024-06-21 00:17:08 +0700

    refactor: :art: update structure folder and start from 0 again with tailwindcss / styled components

--- sharehub-client/src/components/atomic/index.js
+++
@@ -1,9 +0,0 @@
-import Avatar from "./avatar"
-import Button from "./button"
-import Card from "./card"
-import Input from "./input"
-import Modal from "./modal"
-import Backdrop from "./backdrop"
-import Maps from "./maps"
-
-export { Avatar, Button, Card, Input, Modal, Backdrop, Maps }
